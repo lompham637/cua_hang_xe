9f4b7a2bb3578ea0c3741607f72d0f6f9f46dbfb
lompham637 <vukhachuylinh0@gmail.com>
2023-08-15 15:50:04 +0700

    Cap nhat API

--- car_show/components/CarDetails.tsx
+++ car_show/components/CarDetails.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import { CarProps } from '@/Types';
 
-const CarDetails = () => {
+interface CarDetailsProp  {
+    isOpen: boolean;
+    closeModal: () => void;
+    car: CarProps
+}
+
+const CarDetails = ({isOpen, closeModal, car}: CarDetailsProp) => {
   return (
     <div>CarDetails</div>
   )
