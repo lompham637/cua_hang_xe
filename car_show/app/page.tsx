9b64b66241c6f2f2029122af75b9295475fc1a00
lompham637 <vukhachuylinh0@gmail.com>
2023-08-18 16:46:07 +0700

    Cap nhat tim kiem va sap xep

--- car_show/app/page.tsx
+++ car_show/app/page.tsx
@@ -1,11 +1,20 @@
 import Image from 'next/image'
 import { CustomFilter, Hero, SearchBar, CarCard } from '@/components'
-import { fetchCar } from '@/utils';
+import { fetchCars } from '@/utils';
+import { yearsOfProduction, fuels } from '@/constants';
 
-export default async function Home() {
+export default async function Home({ searchParams}) {
 
-  const allCars = await fetchCar();
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || '',
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || '',
+    limit: searchParams.limit || 10,
+    model: searchParams.model || '',
+  });
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
+
 
   return (
     <main className="overflow-hidden">
@@ -21,8 +30,8 @@ export default async function Home() {
         <div className="home__filters">
           <SearchBar />
           <div className="home__filter-container">
-            <CustomFilter title="fuel"/>
-            <CustomFilter title="year"/>
+            <CustomFilter title="fuel" options={fuels}/>
+            <CustomFilter title="year" options={yearsOfProduction} />
           </div>
         </div>
 
--- car_show/components/SearchBar.tsx
+++ car_show/components/SearchBar.tsx
@@ -1,24 +1,93 @@
-"use client"
+"use client";
 
-import { useState } from 'react'
-import { SeacrhManuFacturer } from "."
+import Image from "next/image";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+
+import { SeacrhManuFacturer } from ".";
+
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+  <button type='submit' className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image
+      src={"/magnifying-glass.svg"}
+      alt={"magnifying glass"}
+      width={40}
+      height={40}
+      className='object-contain'
+    />
+  </button>
+);
 
 const SearchBar = () => {
+  const [manufacturer, setManuFacturer] = useState("");
+  const [model, setModel] = useState("");
 
-  const [manufacturer, setManufacturer]  = useState('')
+  const router = useRouter();
 
-  const handleSearch = () => {}
-  
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (manufacturer.trim() === "" && model.trim() === "") {
+      return alert("Please provide some input");
+    }
+
+    updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase());
+  };
+
+  const updateSearchParams = (model: string, manufacturer: string) => {
+    // Tạo đối tượng URLSearchParams mới với các tham số trên URL hiện tại
+    const searchParams = new URLSearchParams(window.location.search);
+
+    // Cập nhật hoặc xóa tham số tìm kiếm 'loại xe' dựa trên giá trị 'loại xe' mà ta tìm
+    if (model) {
+      searchParams.set("model", model);
+    } else {
+      searchParams.delete("model");
+    }
+
+    // Cập nhật hoặc xóa tham số tìm kiếm 'hãng xe' dựa trên giá trị 'hãng xe' mà ta tìm
+    if (manufacturer) {
+      searchParams.set("manufacturer", manufacturer);
+    } else {
+       searchParams.delete("manufacturer");
+    }
+
+    // Tạo biến để nhận giá trị URL mới sau khi tìm kiếm
+    const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
+
+    router.push(newPathname);
+  };
+
   return (
-    <form className="searchbar" onSubmit={handleSearch}>
-        <div className="searchbar__item">
-            <SeacrhManuFacturer 
-                manufacturer={manufacturer}
-                setManufacturer={setManufacturer}
-            />
-        </div>
-    </form>  
-  )
-}
+    <form className='searchbar' onSubmit={handleSearch}>
+      <div className='searchbar__item'>
+        <SeacrhManuFacturer
+          manufacturer={manufacturer}
+          setManufacturer={setManuFacturer}
+        />
+        <SearchButton otherClasses='sm:hidden' />
+      </div>
+      <div className='searchbar__item'>
+        <Image
+          src='/model-icon.png'
+          width={25}
+          height={25}
+          className='absolute w-[20px] h-[20px] ml-4'
+          alt='car model'
+        />
+        <input
+          type='text'
+          name='model'
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder='Tiguan...'
+          className='searchbar__input'
+        />
+        <SearchButton otherClasses='sm:hidden' />
+      </div>
+      <SearchButton otherClasses='max-sm:hidden' />
+    </form>
+  );
+};
 
-export default SearchBar
+export default SearchBar;
